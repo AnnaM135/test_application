f05efb2a4c69f122a7b611a72d8fe0650fa259a7
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2022-12-05 02:46:35 +0400

    feat: changing menu styles with mobile version

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import { menuData } from '../../data/menu';
-import { MenuItem } from '../../elements/MenuItem/MenuItem';
-import _ from './navbar.module.scss';
+import { useState } from "react";
+import { menuData } from "../../data/menu";
+import { MenuItem } from "../../elements/MenuItem/MenuItem";
+import _ from "./navbar.module.scss";
 
 export function Navbar() {
-    return (
-        <nav className={ _._ }>
-            <ul className={ _.__list }>
-                {menuData.map((menu) => (
-                    <MenuItem key={menu.id} menu={menu} />
-                ))}
-            </ul>
-        </nav>
-    )
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <nav className={_._}>
+      <ul
+        className={isMobile ? _.__mobile : _.__list}
+        onClick={() => setIsMobile(false)}
+      >
+        {menuData.map((menu) => (
+          <MenuItem key={menu.id} menu={menu} />
+        ))}
+      </ul>
+      <div className={_.__icon} onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? (
+          <div className={_.__icon_close}></div>
+        ) : (
+          <div className={_.__icon_mobile}>
+            <div className={_.__icon_mobile_trans}></div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
