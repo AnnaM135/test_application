0db1db1e6b9d5bf422852087d9440d1fc442057a
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2022-12-04 17:18:06 +0400

    style: add mobile version for about product section

--- src/components/AboutProduct/AboutProduct.jsx
+++ src/components/AboutProduct/AboutProduct.jsx
@@ -1,22 +1,19 @@
 import _ from "./aboutProduct.module.scss";
 
-import Nootris from '../../assets/images/nootris.png';
-import Ginger from '../../assets/images/ginger.png';
-import Lemon from '../../assets/images/Lemon.png';
-
+import Nootris from "../../assets/images/nootris.png";
+import Ginger from "../../assets/images/ginger.png";
+import Lemon from "../../assets/images/Lemon.png";
 
 export function AboutProduct() {
   return (
     <section className={_._}>
       <div className={`${_.__content} container`}>
         <div className={_.__feautures}>
-          <div className={_.__feautures_top}>
-            <h1 className={_.__feautures_head}>АКТИВИРУЙ ИММУНИТЕТ!</h1>
-            <p className={_.__feautures_description}>
-              Всего пять секунд в день помогут укрепить иммунитет и повысить
-              защитные силы организма
-            </p>
-          </div>
+          <h1 className={_.__feautures_head}>АКТИВИРУЙ ИММУНИТЕТ!</h1>
+          <p className={_.__feautures_description}>
+            Всего пять секунд в день помогут укрепить иммунитет и повысить
+            защитные силы организма
+          </p>
           <div className={_.__feautures_bottom}>
             <h2 className={_.__feautures_title}>NOOTRIS ПОМОГАЕТ</h2>
             <p className={_.__feautures_quotes}>
@@ -25,15 +22,15 @@ export function AboutProduct() {
           </div>
         </div>
         <div className={_.__compositionImages}>
-            <div className={_.__compositionImages_nootris}>
-                <img src={Nootris} alt="Nootris"/>
-            </div>
-            <div className={_.__compositionImages_ginger}>
-                <img src={Ginger} alt="Ginger"/>
-            </div>
-            <div className={_.__compositionImages_lemon}>
-                <img src={Lemon} alt="Lemon"/>
-            </div>
+          <div className={_.__compositionImages_nootris}>
+            <img src={Nootris} alt="Nootris" />
+          </div>
+          <div className={_.__compositionImages_ginger}>
+            <img src={Ginger} alt="Ginger" />
+          </div>
+          <div className={_.__compositionImages_lemon}>
+            <img src={Lemon} alt="Lemon" />
+          </div>
         </div>
       </div>
     </section>
